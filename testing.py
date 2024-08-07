c917fe1126464ee9246844f36eee353a10cfbb31
Mehmet Sanisoglu <sanisoglumehmet@gmail.com>
2024-08-07 12:41:23 +0300

    Draft

--- testing.py
+++ testing.py
@@ -2,10 +2,10 @@ import torch
 print("Is cuda available?")
 print("Yes" if torch.cuda.is_available() else "No")
 
-torch.cuda.device_count()
+print(torch.cuda.device_count())
 
-torch.cuda.current_device()
+print(torch.cuda.current_device())
 
-torch.cuda.device(0)
+print(torch.cuda.device(0))
 
-torch.cuda.get_device_name(0)
+print(torch.cuda.get_device_name(0))
