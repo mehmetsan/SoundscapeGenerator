9658504cb3cba7dc688f9c15fd8f2bc2ebf478f3
Mehmet Sanisoglu <sanisoglumehmet@gmail.com>
2024-08-09 16:28:22 +0300

    Scripts refactored

--- load_download_model.py
+++ load_download_model.py
@@ -1,7 +1,9 @@
 import os
+import torch
 import torch.nn as nn
-from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionPipeline, DDPMScheduler
 from torch.utils.data import DataLoader
+from torch.optim import AdamW
 
 from utils.training_utils import CustomImageDataset
 
@@ -31,3 +33,56 @@ try:
     print('Dataset loading successful')
 except Exception as e:
     raise Exception(f"Model loading failed due to {e}")
+
+# Try training
+try:
+    if not torch.cuda.is_available():
+        raise Exception("Cuda is not available, aborted training")
+
+    # Move model to device
+    device = "cuda"
+    model.to(device)
+    text_encoder.to(device)
+
+    print(f"\nThere are {torch.cuda.device_count()} available devices")
+
+    # Training settings
+    num_epochs = 1
+    learning_rate = 1e-4
+    scheduler = DDPMScheduler(beta_start=0.0001, beta_end=0.02, num_train_timesteps=500)
+    optimizer = AdamW(model.parameters(), lr=learning_rate)
+
+    print('Started training')
+    # Training loop
+    for epoch in range(num_epochs):
+        print(f"Epoch: {epoch}")
+        for images, labels in dataloader:
+            images = images.to(device)
+
+            # Generate text embeddings
+            text_inputs = [f"sndscp {dataset.classes[label]}" for label in labels]
+            text_inputs = tokenizer(text_inputs, return_tensors="pt", padding=True, truncation=True).input_ids
+            text_embeddings = text_encoder(text_inputs.to(device)).last_hidden_state
+
+            # Forward pass
+            noise = torch.randn_like(images)
+            time_steps = torch.randint(0, scheduler.num_train_timesteps, (images.shape[0],),
+                                       device=images.device).long()
+            noisy_images = scheduler.add_noise(images, noise, time_steps)
+
+            model_output = model(noisy_images, time_steps, text_embeddings)["sample"]
+
+            print(f"Model output is {model_output}")
+            # Compute loss (simplified)
+            loss = torch.nn.functional.mse_loss(model_output, noise)
+
+            # Backward pass and optimization
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print(f"Epoch {epoch + 1}/{num_epochs} - Loss: {loss.item()}")
+
+    print('Finished training')
+except Exception as e:
+    raise Exception(f"Training failed due to {e}")
