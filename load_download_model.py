fcb7810a7795e3138dc22a5cd354e3ed39a19828
Mehmet Sanisoglu <sanisoglumehmet@gmail.com>
2024-08-12 16:50:56 +0300

    alternate training method

--- load_download_model.py
+++ load_download_model.py
@@ -1,21 +1,19 @@
 import os
 import torch.nn as nn
 from diffusers import StableDiffusionPipeline
+from env_variables import model_cache_path
 
-model_cache_dir = '/ext/sanisoglum/checkpoints/caches'
-
-if os.path.exists(os.path.join(model_cache_dir, 'models--riffusion--riffusion-model-v1')):
+if os.path.exists(os.path.join(model_cache_path, 'models--riffusion--riffusion-model-v1')):
     print('A cache already exists, either resuming download or skipping download')
 else:
     print('Downloading model from scratch')
-    os.makedirs(model_cache_dir, exist_ok=True)
+    os.makedirs(model_cache_path, exist_ok=True)
 
 pipeline = StableDiffusionPipeline.from_pretrained("riffusion/riffusion-model-v1",
-                                                   cache_dir=model_cache_dir,
+                                                   cache_dir=model_cache_path,
                                                    resume_download=True)
 
 model = pipeline.unet
-model = nn.DataParallel(model)
 text_encoder = pipeline.text_encoder
 tokenizer = pipeline.tokenizer
 
