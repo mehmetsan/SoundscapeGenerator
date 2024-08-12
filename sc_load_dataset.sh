591b52cedd20b8497b088078e9dcd034db2dfd1a
Mehmet Sanisoglu <sanisoglumehmet@gmail.com>
2024-08-12 17:23:19 +0300

    Load test

---
+++ sc_load_dataset.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name load_download_riffusion_model
+#SBATCH --output log/%j_out.txt
+#SBATCH --error log/%j_err.txt
+#SBATCH --mail-user mehmet.sanisoglu@studenti.unipd.it
+#SBATCH --mail-type ALL
+#SBATCH --time 2-20:00:00
+#SBATCH --ntasks 1
+#SBATCH --partition allgroups
+#SBATCH --mem 16G
+#SBATCH --gres=gpu:rtx:2
+
+# description: Slurm job to download or verify loading the riffusion model checkpoint
+# author: Mehmet Sanisoglu
+
+source /home/sanisoglum/miniconda3/bin/activate my_env
+
+WORKDIR=/home/sanisoglum/SoundscapeGenerator
+cd "$WORKDIR" || exit 0  # Create and change to the specified directory
+
+export CUDA_LAUNCH_BLOCKING=1
+
+srun python load_dataset.py
+
