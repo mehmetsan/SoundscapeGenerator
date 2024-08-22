f3efe3176358a7092ebd6028841d5f99eb4bf478
Mehmet Sanisoglu <sanisoglumehmet@gmail.com>
2024-08-22 03:18:41 +0300

    Added wandb log loss to training

--- utils/riffusion_pipeline.py
+++ utils/riffusion_pipeline.py
@@ -10,6 +10,7 @@ import typing as T
 
 import numpy as np
 import torch
+import diffusers
 from diffusers.models import AutoencoderKL, UNet2DConditionModel
 from diffusers.pipeline_utils import DiffusionPipeline
 from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
