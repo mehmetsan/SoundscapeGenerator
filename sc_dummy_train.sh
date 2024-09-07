b392d08ecd51e6851d4e249169fa140a5a43f1e6
Mehmet Sanisoglu <sanisoglumehmet@gmail.com>
2024-09-07 21:27:27 +0200

    torchrun

--- sc_dummy_train.sh
+++ sc_dummy_train.sh
@@ -18,13 +18,15 @@ source /home/sanisoglum/miniconda3/bin/activate my_env
 WORKDIR=/home/sanisoglum/SoundscapeGenerator
 cd "$WORKDIR" || exit 0  # Create and change to the specified directory
 
+lsof -i :29501
+
 # No need to set CUDA_VISIBLE_DEVICES, torchrun handles GPU allocation
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 export HYDRA_FULL_ERROR=1
 export WANDB_DEBUG=true
 export WANDB_HTTP_TIMEOUT=60  # Increase timeout
 export MASTER_ADDR=$(hostname)  # Set the master address to the current node's hostname
-export MASTER_PORT=$(shuf -i 20000-60000 -n 1)        # Use a specific port for inter-process communication
+export MASTER_PORT=29501        # Use a specific port for inter-process communication
 
 # Use torchrun to launch the distributed training
 torchrun --nproc_per_node=2 alternate_train.py  # Use 2 processes (one per GPU)
