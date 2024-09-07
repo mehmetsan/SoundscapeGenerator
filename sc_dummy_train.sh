990d0f3899575a008ae65d0ad1f436b92675e371
Mehmet Sanisoglu <sanisoglumehmet@gmail.com>
2024-09-07 14:34:19 +0200

    More gpus

--- sc_dummy_train.sh
+++ sc_dummy_train.sh
@@ -8,7 +8,7 @@
 #SBATCH --ntasks 1
 #SBATCH --partition allgroups
 #SBATCH --mem 36G
-#SBATCH --gres=gpu:rtx:2
+#SBATCH --gres=gpu:rtx:3
 
 # description: Slurm job to train the riffusion model with emotion tags
 # author: Mehmet Sanisoglu
