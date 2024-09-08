2bc368758f8305363d76b241940bc558e29fa0fe
Mehmet Sanisoglu <sanisoglumehmet@gmail.com>
2024-09-09 00:56:49 +0200

    Dataparallel try

--- sc_train.sh
+++ sc_train.sh
@@ -7,7 +7,7 @@
 #SBATCH --time 2-20:00:00
 #SBATCH --ntasks 1  # One task per node
 #SBATCH --gpus-per-node=2  # Use 2 GPUs per node
-#SBATCH --gres=gpu:a40:2  # Request 2 A40 GPUs
+#SBATCH --gres=gpu:a40:2  # Request 2 A40 GPUs explicitly
 #SBATCH --partition allgroups
 #SBATCH --mem 20G
 
@@ -18,7 +18,7 @@ cd "$WORKDIR" || exit 0  # Create and change to the specified directory
 
 export CUDA_LAUNCH_BLOCKING=1
 export NCCL_DEBUG=INFO  # Enable NCCL debugging info
-export NCCL_P2P_LEVEL=NVL  # Use local P2P communication only (intra-node)
+export NCCL_P2P_LEVEL=NVL  # Limit P2P communication to local GPUs
 # export NCCL_SOCKET_IFNAME=eth0  # Uncomment and set your network interface if needed
 
 srun python -m torch.distributed.launch --nproc_per_node=2 --use_env train_model.py
