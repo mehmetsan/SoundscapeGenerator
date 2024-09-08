d3cfa22239d0383ebed90534c3d424d2cbd2bffb
Mehmet Sanisoglu <sanisoglumehmet@gmail.com>
2024-09-09 01:22:35 +0200

    Dataparallel try from stage

--- sc_train.sh
+++ sc_train.sh
@@ -5,25 +5,18 @@
 #SBATCH --mail-user mehmet.sanisoglu@studenti.unipd.it
 #SBATCH --mail-type ALL
 #SBATCH --time 2-20:00:00
-#SBATCH --ntasks 1  # Keep 1 task per node
-#SBATCH --gres=gpu:a40:2  # Request 2 GPUs per node
+#SBATCH --nodes=1  # Force the job to run on a single node
+#SBATCH --gres=gpu:a40:2  # Request 2 GPUs on the same node
 #SBATCH --partition allgroups
 #SBATCH --mem 20G
-#SBATCH --gres=gpu:a40:2
-
-# description: Slurm job to train the riffusion model with emotion tags
-# author: Mehmet Sanisoglu
-
 
 source /home/sanisoglum/miniconda3/bin/activate my_env
 
 WORKDIR=/home/sanisoglum/SoundscapeGenerator
 cd "$WORKDIR" || exit 0  # Change to the specified directory
 
-export NCCL_SOCKET_IFNAME=eth0  # Or the correct network interface for your system
 export NCCL_DEBUG=INFO
-export CUDA_LAUNCH_BLOCKING=1
-export NCCL_P2P_LEVEL=NVL
-
+export NCCL_P2P_LEVEL=NVL  # Limit to intra-node communication
+# export NCCL_SOCKET_IFNAME=eth0  # Leave this out for now to see if intra-node works without it
 
 srun python -m torch.distributed.launch --nproc_per_node=2 --use_env train_model.py
