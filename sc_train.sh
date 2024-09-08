07c80b11bc2dcc8d00457dbb6fa2833158363a6a
Mehmet Sanisoglu <sanisoglumehmet@gmail.com>
2024-09-09 01:13:46 +0200

    Dataparallel try from stage

--- sc_train.sh
+++ sc_train.sh
@@ -20,6 +20,9 @@ source /home/sanisoglum/miniconda3/bin/activate my_env
 WORKDIR=/home/sanisoglum/SoundscapeGenerator
 cd "$WORKDIR" || exit 0  # Change to the specified directory
 
+export NCCL_DEBUG=INFO
 export CUDA_LAUNCH_BLOCKING=1
+export NCCL_P2P_LEVEL=NVL
+
 
 srun python -m torch.distributed.launch --nproc_per_node=2 --use_env train_model.py
